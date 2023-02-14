ddf24af3e499fb888625428b25b64bec625057f4
yayang12 <yayang1@s.sfusd.edu>
2023-02-14 10:56:32 -0800

    Finished Unit 3.1

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
 {
+    private float speed = 30;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class MoveLeft : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
     }
 }
